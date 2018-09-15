e021750f49a3c9a5c325beed9fd69b1d7efb3493
Marco N <sainoba@gmail.com>
2018-09-15 12:34:59 -0500

    Coded basic skeleton for the processor

--- rp2a03/processor.py
+++ rp2a03/processor.py
@@ -1,2 +1,18 @@
 class Processor:
-    pass
+    def __init__(self, registers, memory):
+        self.registers = registers
+        self.memory = memory
+
+    def step(self) -> dict:
+        opcode = self._fetch()
+        _function = self._decode(opcode)
+        cycles = self._execute(_function)
+        return {"cycles": cycles, "opcode": opcode}
+
+    def _fetch(self) -> int:
+        instruction = self.memory[self.registers.program_counter]
+        self.registers.program_counter += 1
+        return instruction
+
+    def _decode(self, opcode: int) -> function:
+        return lambda: None
