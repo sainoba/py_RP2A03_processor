9ef043efd7cdddfcfc5eff7a311faa58b73ca304
Marco N <sainoba@gmail.com>
2018-09-14 23:05:51 -0500

    Renamed register object, and added registers to the Registers class

--- rp2a03/registers.py
+++ rp2a03/registers.py
@@ -1,3 +1,6 @@
+from ctypes import c_int8, c_uint8, c_uint16
+
+
 class Registers:
     def __init__(self):
         self.program_counter = 0
@@ -6,3 +9,77 @@ class Registers:
         self.accumulator = 0
         self.x = 0
         self.y = 0
+
+    def __setattr__(self, name, value):
+        if name in ["x", "y", "accumulator"]:
+            value = c_int8(value).value
+        elif name in ["status", "stack_pointer"]:
+            value = c_uint8(value).value
+        elif name is "program_counter":
+            value = c_uint16(value).value
+        super().__setattr__(name, value)
+
+    @staticmethod
+    def _get_flag(flag: int, position: int) -> int:
+        return flag >> position & 1
+
+    @staticmethod
+    def _set_flag(flag: int, position: int, value: int) -> int:
+        flag &= ~(1 << position)
+        return flag | (value << position)
+
+    @property
+    def carry_flag(self) -> int:
+        return self._get_flag(self.status, 0)
+
+    @carry_flag.setter
+    def carry_flag(self, value: int):
+        self.status = self._set_flag(self.status, 0, value)
+
+    @property
+    def zero_flag(self) -> int:
+        return self._get_flag(self.status, 1)
+
+    @zero_flag.setter
+    def zero_flag(self, value: int):
+        self.status = self._set_flag(self.status, 1, value)
+
+    @property
+    def interrupt_disable(self) -> int:
+        return self._get_flag(self.status, 2)
+
+    @interrupt_disable.setter
+    def interrupt_disable(self, value: int):
+        self.status = self._set_flag(self.status, 2, value)
+
+    @property
+    def decimal_mode(self) -> int:
+        return self._get_flag(self.status, 3)
+
+    @decimal_mode.setter
+    def decimal_mode(self, value: int):
+        self.status = self._set_flag(self.status, 3, value)
+
+    @property
+    def break_command(self) -> int:
+        return self._get_flag(self.status, 4)
+
+    @break_command.setter
+    def break_command(self, value: int):
+        self.status = self._set_flag(self.status, 4, value)
+
+    @property
+    def overflow_flag(self) -> int:
+        return self._get_flag(self.status, 6)
+
+    @overflow_flag.setter
+    def overflow_flag(self, value: int):
+        self.status = self._set_flag(self.status, 6, value)
+
+    @property
+    def negative_flag(self) -> int:
+        return self._get_flag(self.status, 7)
+
+    @negative_flag.setter
+    def negative_flag(self, value: int):
+        self.status = self._set_flag(self.status, 7, value)
--- tests/test_registers.py
+++ tests/test_registers.py
@@ -3,47 +3,63 @@ from rp2a03.registers import Registers
 
 
 @pytest.fixture
-def register():
+def registers():
     return Registers()
 
 
-def test_carry_flag(register: Registers):
-    assert register.carry_flag == 0
-    register.carry_flag = 1
-    assert register.carry_flag == 1
+def test_carry_flag(registers: Registers):
+    assert registers.carry_flag == 0
+    registers.carry_flag = 1
+    assert registers.carry_flag == 1
 
 
-def test_zero_flag(register):
-    assert register.zero_flag == 0
-    register.zero_flag = 1
-    assert register.zero_flag == 1
+def test_zero_flag(registers: Registers):
+    assert registers.zero_flag == 0
+    registers.zero_flag = 1
+    assert registers.zero_flag == 1
 
 
-def test_interrupt_disable(register):
-    assert register.interrupt_disable == 0
-    register.interrupt_disable = 1
-    assert register.interrupt_disable == 1
+def test_interrupt_disable(registers: Registers):
+    assert registers.interrupt_disable == 0
+    registers.interrupt_disable = 1
+    assert registers.interrupt_disable == 1
 
 
-def test_decimal_mode(register):
-    assert register.decimal_mode == 0
-    register.decimal_mode = 1
-    assert register.decimal_mode == 1
+def test_decimal_mode(registers: Registers):
+    assert registers.decimal_mode == 0
+    registers.decimal_mode = 1
+    assert registers.decimal_mode == 1
 
 
-def test_break_command(register):
-    assert register.break_command == 0
-    register.break_command = 1
-    assert register.break_command == 1
+def test_break_command(registers: Registers):
+    assert registers.break_command == 0
+    registers.break_command = 1
+    assert registers.break_command == 1
 
 
-def test_overflow_flag(register):
-    assert register.overflow_flag == 0
-    register.overflow_flag = 1
-    assert register.overflow_flag == 1
+def test_overflow_flag(registers: Registers):
+    assert registers.overflow_flag == 0
+    registers.overflow_flag = 1
+    assert registers.overflow_flag == 1
 
 
-def test_negative_flag(register):
-    assert register.negative_flag == 0
-    register.negative_flag = 1
-    assert register.negative_flag == 1
+def test_negative_flag(registers: Registers):
+    assert registers.negative_flag == 0
+    registers.negative_flag = 1
+    assert registers.negative_flag == 1
+
+
+def test_flags_dont_affect_others(registers: Registers):
+    unset_flags = ["carry_flag", "zero_flag", "interrupt_disable",
+                   "decimal_mode", "break_command", "overflow_flag",
+                   "negative_flag"]
+    set_flags = []
+    assert registers.status == 0
+    while unset_flags:
+        flag = unset_flags.pop()
+        setattr(registers, flag, 1)
+        set_flags.append(flag)
+        for flag in unset_flags:
+            assert getattr(registers, flag) == 0, "{} should be unset".format(flag)
+        for flag in set_flags:
+            assert getattr(registers, flag) == 1, "{} should be set".format(flag)
